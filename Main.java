fed7a7cdae85a22eadd0bd97f1c2ac8351e4ebea
grammarandsyntax <144465898+grammarandsyntax@users.noreply.github.com>
2023-09-11 10:08:18 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,15 +3,19 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("hello world");
-        System.out.print("drjava sucks");
+        System.out.println("drjava sucks");
 
         //variables
         int inty=46;
         double floaty=-99.9;
-        boolean period3=True;
+        final boolean period3= true; //cannot be changed
 
         System.out.println(inty);
         System.out.println(floaty);
         System.out.println(period3);
+        //test
+        System.out.print("I have ");
+        System.out.print(inty);
+        System.out.println(" horses");
     }
 }
