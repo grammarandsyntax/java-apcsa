ab9cf781956340facdd9d0a9d1780526cbd900ee
grammarandsyntax <144465898+grammarandsyntax@users.noreply.github.com>
2023-09-11 09:55:19 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,14 @@ public class Main
     {
         System.out.println("hello world");
         System.out.print("drjava sucks");
+
+        //variables
+        int inty=46;
+        double floaty=-99.9;
+        boolean period3=True;
+
+        System.out.println(inty);
+        System.out.println(floaty);
+        System.out.println(period3);
     }
 }
