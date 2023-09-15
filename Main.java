f5663e9b1a2322faea2344d6751365fdb61aa0ca
grammarandsyntax <yevak2@nycstudents.net>
2023-09-14 20:11:37 -0400

    Rewrote program to have coherent theme & operators

--- Main.java
+++ Main.java
@@ -1,38 +1,52 @@
 import java.util.Scanner;
+import java.util.Date;
 public class Main
 {
+    static void Wait()
+    {
+        try {
+            Thread.sleep(4000);
+        } catch(InterruptedException e){ 
+            Thread.currentThread().interrupt();
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("hello world");
-        System.out.println("drjava sucks");
+        //Lancet section
+        int numLmd = 340785;
+        int numOrundum = 29310;
+        final boolean isSixStar = true; //only used when six star op is mentioned (texas?)
+        double redeploymentTime = 18.5;
+        int numSixStarOperators = 0;
 
-        //variables
-        int inty=46;
-        double floaty=-99.9;
-        final boolean period3= true; //cannot be changed
+        System.out.println("What is your name?");
+        Scanner scan = new Scanner(System.in); //scanner opens
+        String name=scan.nextLine();
+        System.out.println("Hello Dr. "+name+". I am Lancet-2, model Exister-S62, and I will be subbing in for miss Amiya for the next few days.");
+        scan.close(); //scanner closes
+        Wait();
+        System.out.println("I'll start off with a recording of your current LMD and Orundum counts.");
+        System.out.println("Calibrating...");
+        Wait();
+        System.out.println("You currently have "+numLmd+" LMD and "+numOrundum+" Orundum.");
+        System.out.println("You wish to search for new operators? I suppose I can't stop you...");
+        System.out.println("Recalibrating orundum amount...");
+        Wait();
+        numOrundum=numOrundum-6000; //10 pull lol
+        System.out.println("You now have "+numOrundum+" Orundum. And it looks like you've hired miss Texas! However, she'd like some compensation in LMD for her work; about half of our current amount.");
+        Wait();
+        System.out.println("You accept the charges? Alright, I'll send the requested LMD over to her.");
+        numLmd/=2;
+        numSixStarOperators++;
+        System.out.println("There is now "+numLmd+" LMD in our holdings, and your current number of Top Operators is "+numSixStarOperators+".");
+        Wait();
+        System.out.println("Doctor, you wish to learn more about Texas? Well, after returning from her mission in Siracusa, it is "+isSixStar+" that she has gained more experience in battle. Her redeployment time has been reduced heavily to "+redeploymentTime+" seconds. It increases by "+(redeploymentTime%2.0)+" seconds on each new redeployment.");
+        Wait();
+        Wait();
+        System.out.println("Wait, you want to hire more operators already?! Doctor, please let me recompensate the missing amount with some mining operations..");
+        numOrundum=(numOrundum-6000)*2;
 
-        System.out.println(inty);
-        System.out.println(floaty);
-        System.out.println(period3);
-        //test
-        System.out.print("I have ");
-        System.out.print(inty);
-        System.out.println(" horses");
-        System.out.println("");
-        //1.3
-        int day=1;
-        double cash=99.99;
-        System.out.println("On day "+day+" I have $"+cash);
-        day=day+1;
-        cash=cash/2;
-        System.out.println("enter your address");
-        Scanner scan=new Scanner(System.in);
-        String question=scan.nextLine();
-        System.out.println("I will find you at "+question);
-        System.out.println("How much cash do you have?");
-        String cashAmount=scan.nextDouble();
-        System.out.println("Give me $"+cashAmount);
-        scan.close();
-        
+
     }
 }
