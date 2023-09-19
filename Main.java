e0936db83953d7d644991840efc59ea7362b53c8
grammarandsyntax <yevak2@nycstudents.net>
2023-09-19 10:18:07 -0400

    Added casting

--- Main.java
+++ Main.java
@@ -24,35 +24,66 @@ public class Main
         Scanner scan = new Scanner(System.in); //scanner opens
         String name=scan.nextLine();
         System.out.println("Hello Dr. "+name+". I am Lancet-2, model Exister-S62, and I will be subbing in for miss Amiya for the next few days.");
-        scan.close(); //scanner closes
+         //scanner closes
+
         Wait();
+
         System.out.println("I'll start off with a recording of your current LMD and Orundum counts.");
         System.out.println("Calibrating...");
+
         Wait();
+
         System.out.println("You currently have "+numLmd+" LMD and "+numOrundum+" Orundum.");
         System.out.println("You wish to search for new operators? I suppose I can't stop you...");
         System.out.println("Recalibrating orundum amount...");
-        Wait();
         numOrundum=numOrundum-6000; //10 pull lol
-        System.out.println("You now have "+numOrundum+" Orundum. And it looks like you've hired miss Texas! However, she'd like some compensation in LMD for her work; about half of our current amount.");
+
         Wait();
+
+        System.out.println("You now have "+numOrundum+" Orundum. And it looks like you've hired miss Texas! However, she'd like some compensation in LMD for her work; about half of our current amount.");
+
+        Wait();
+
         System.out.println("You accept the charges? Alright, I'll send the requested LMD over to her.");
         numLmd/=2;
         numSixStarOperators++;
         System.out.println("There is now "+numLmd+" LMD in our holdings, and your current number of Top Operators is "+numSixStarOperators+".");
+
         Wait();
+
         System.out.println("Doctor, you wish to learn more about Texas? Well, after returning from her mission in Siracusa, it is "+isSixStar+" that she has gained more experience in battle. Her redeployment time has been reduced heavily to "+redeploymentTime+" seconds. It increases by "+(redeploymentTime%4)+" seconds on each new redeployment.");
+
         Wait();
         Wait();
+
         System.out.println("Wait, you want to hire more operators already?! Doctor, please let me recompensate the missing amount with some mining operations. I'll be back tomorrow! In the meantime, please sort out miss Texas's files.");
         numOrundum=(numOrundum-6000)*2;
+
         Wait();
+
         System.out.println("");
         System.out.println("(One day later..)");
         System.out.println("");
-        Wait();
-        System.out.println("I hope I'm not interrupting, Doctor, but I've just returned with "+(numOrundum-(numOrundum/2))+" Orundum. We now hold "+numOrundum+" Orundum total.");
 
+        Wait();
+
+        System.out.println("I hope I'm not interrupting, Doctor, but I've just returned with "+(numOrundum-(numOrundum/2))+" Orundum. We now hold "+numOrundum+" Orundum total.");
+        System.out.println("I've also overheard a fight occurring over operators not being able to divide food rations equally amongst themselves. Would you like to help sort it out?");
+
+        System.out.println("(How many whole pounds of food will you allocate?)");
+        scan = new Scanner(System.in); //scanner reuse
+        int poundsFood = scan.nextInt();
+        System.out.println("(Between how many operators?)");
+        scan = new Scanner(System.in); //scanner reuse
+        int numOperatorsFood = scan.nextInt();
+        scan.close(); //scanner closes
+
+        try { //division
+            System.out.println("Ok, so we need to give each operator "+((double)poundsFood/numOperatorsFood)+" pounds of food."); //add truncation thing with mod
+        }
+        catch (ArithmeticException error){
+        System.out.print("Please don't use zero for this input, doctor!");
+        }
 
     }
 }
