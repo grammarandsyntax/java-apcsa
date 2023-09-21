dc1d27987a31aaa4627d0e3e8bb738ae4edc9d84
grammarandsyntax <yevak2@nycstudents.net>
2023-09-21 10:06:10 -0400

    Added classes

---
+++ Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+    private String name;
+    private int rarity;
+    private int potential;
+    private int attack;
+    private int defense;
+    private double redeploymentTime;
+
+
+}
---
+++ Orundum.java
@@ -0,0 +1,4 @@
+public class Orundum {
+    private int quantity;
+
+}
