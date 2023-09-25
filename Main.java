41052ca5f8f36dddaf835bc237a91b8786bc7e83
grammarandsyntax <yevak2@nycstudents.net>
2023-09-25 17:59:48 -0400

    added if, elif, else, while

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-import java.util.Date;
-public class Main
-{
-    static void Wait()
+public class Main {
+
+    static void Wait(int milliseconds)
     {
         try {
-            Thread.sleep(4000);
+            Thread.sleep(milliseconds);
         } catch(InterruptedException e){ 
             Thread.currentThread().interrupt();
         }
@@ -13,77 +12,126 @@ public class Main
 
     public static void main(String[] args)
     {
-        //Lancet section
+        //vars
+
         int numLmd = 340785;
         int numOrundum = 29310;
         final boolean isSixStar = true; //only used when six star op is mentioned (texas?)
-        double redeploymentTime = 18.5;
         int numSixStarOperators = 0;
 
-        System.out.println("What is your name?");
+        //Amiya begin
+
+        System.out.println("Please input your name:");
         Scanner scan = new Scanner(System.in); //scanner opens
         String name=scan.nextLine();
-        System.out.println("Hello Dr. "+name+". I am Lancet-2, model Exister-S62, and I will be subbing in for miss Amiya for the next few days.");
-         //scanner closes
+        System.out.println("Good morning Dr. "+name+". I hope you slept well, we've got a lot of work ahead of us!");
 
-        Wait();
+        Wait(3000);
 
         System.out.println("I'll start off with a recording of your current LMD and Orundum counts.");
-        System.out.println("Calibrating...");
 
-        Wait();
+        Wait(2000);
 
         System.out.println("You currently have "+numLmd+" LMD and "+numOrundum+" Orundum.");
-        System.out.println("You wish to search for new operators? I suppose I can't stop you...");
-        System.out.println("Recalibrating orundum amount...");
-        numOrundum=numOrundum-6000; //10 pull lol
-
-        Wait();
-
-        System.out.println("You now have "+numOrundum+" Orundum. And it looks like you've hired miss Texas! However, she'd like some compensation in LMD for her work; about half of our current amount.");
-
-        Wait();
-
-        System.out.println("You accept the charges? Alright, I'll send the requested LMD over to her.");
-        numLmd/=2;
-        numSixStarOperators++;
-        System.out.println("There is now "+numLmd+" LMD in our holdings, and your current number of Top Operators is "+numSixStarOperators+".");
-
-        Wait();
-
-        System.out.println("Doctor, you wish to learn more about Texas? Well, after returning from her mission in Siracusa, it is "+isSixStar+" that she has gained more experience in battle. Her redeployment time has been reduced heavily to "+redeploymentTime+" seconds. It increases by "+(redeploymentTime%4)+" seconds on each new redeployment.");
-
-        Wait();
-        Wait();
-
-        System.out.println("Wait, you want to hire more operators already?! Doctor, please let me recompensate the missing amount with some mining operations. I'll be back tomorrow! In the meantime, please sort out miss Texas's files.");
-        numOrundum=(numOrundum-6000)*2;
-
-        Wait();
-
-        System.out.println("");
-        System.out.println("(One day later..)");
-        System.out.println("");
-
-        Wait();
-
-        System.out.println("I hope I'm not interrupting, Doctor, but I've just returned with "+(numOrundum-(numOrundum/2))+" Orundum. We now hold "+numOrundum+" Orundum total.");
-        System.out.println("I've also overheard a fight occurring over operators not being able to divide food rations equally amongst themselves. Would you like to help sort it out?");
-
-        System.out.println("(How many whole pounds of food will you allocate?)");
+        System.out.println("Do you want to hire operators?");
         scan = new Scanner(System.in); //scanner reuse
-        int poundsFood = scan.nextInt();
-        System.out.println("(Between how many operators?)");
-        scan = new Scanner(System.in); //scanner reuse
-        int numOperatorsFood = scan.nextInt();
-        scan.close(); //scanner closes
+        String askHireOperator = scan.nextLine().toLowerCase();
 
-        try { //division
-            System.out.println("Ok, so we need to give each operator "+((double)poundsFood/numOperatorsFood)+" pounds of food."); //add truncation thing with mod
-        }
-        catch (ArithmeticException error){
-        System.out.print("Please don't use zero for this input, doctor!");
+        //ask to pull
+        if (askHireOperator.equals("yes")){ //want to pull
+            System.out.println("Alright! That'll be 6000 orundum spent, which means... ");
+            numOrundum = numOrundum-6000; //substract
+
+            Wait(3000);
+
+            System.out.println("There's "+numOrundum+" orundum left.");
+            Operator Texas = new Operator();
+            Texas.setName("Texas");
+            Texas.setRedeploymentTime(18.0);
+            Texas.setAttack(219);
+
+            System.out.println(Texas.getName()+" was hired! She would however like some compensation for her work and training. The total amount will be half our current LMD amount.");
+
+            Wait(4500);
+            
+            System.out.println("You accept the charges? Alright, I'll send the LMD to her.");
+            numLmd/=2;
+            numSixStarOperators++;
+            System.out.println("There is now "+numLmd+" LMD in our holdings, and the current number of Top Operators is "+numSixStarOperators+".");     
+
+            Wait(4000);
+
+            System.out.println("Doctor, do you want to learn more about miss Texas?");
+            scan = new Scanner(System.in); //scanner reuse
+            String askInfo = scan.nextLine().toLowerCase();
+
+            if (askInfo.equals("yes")){ //want infodump
+                System.out.println("After returning from her mission in Siracusa, it is "+isSixStar+" that she has gained more experience in battle. Her redeployment time has been reduced heavily to "+Texas.getRedeploymentTime()+" seconds. It increases by "+(Texas.getRedeploymentTime()/4)+" seconds on each new redeployment.");
+                Texas.getAttack();
+
+                Wait(8000);
+            } else {
+                System.out.println("Ok.. but remember, it's important to learn about your operators to learn how to most effectively command them in battle!");
+                Wait(1500);
+                System.out.println("Anyway, I'll go sort out Texas's files.");
+            }
+        } else {
+            System.out.println("That's alright, we have other matters to handle anyway!");
         }
 
+        Wait(3000);
+        System.out.println(" ");
+        System.out.println("A bit later...");
+        System.out.println(" ");
+        Wait(2000);
+
+        //the silly 2
+
+        System.out.println("Doctor, while I was working, I overheard some operators in the cafeteria arguing... could you please help settle it? I think it's over who has to cook..?");
+        System.out.println(" ");
+        System.out.println("Do you choose Gummy or Lava as today's cook?");
+        scan = new Scanner(System.in); //scanner reuse
+        String chooseCook = scan.nextLine().toLowerCase();
+
+        Wait(4000);
+
+        while(true){ //choose bwteen gum and lava
+            if (chooseCook.equals("gummy")){ //YES GOOD
+
+                Wait(3000);
+                System.out.println("It seems the operators like her cooking! How will you divide the food among them though..?");
+                scan = new Scanner(System.in); //scanner reuse
+
+                System.out.println("(How many whole pounds of food will you allocate?)");
+
+                int poundsFood = scan.nextInt();
+
+                System.out.println("(Between how many operators?)");
+
+                scan = new Scanner(System.in); //scanner reuse
+                int numOperatorsFood = scan.nextInt();
+                scan.close(); //scanner closes
+
+                try { //division
+                    System.out.println("Ok, so we need to give each operator "+((double)poundsFood/numOperatorsFood)+" pounds of food."); //add truncation thing with mod
+                }
+                catch (ArithmeticException error){
+                System.out.print("Please don't use zero for this input, doctor!");
+                }
+            }
+            else if (chooseCook.equals("lava")){
+
+                Wait(3000);
+                System.out.println("It seems the operators don't like her cooking... I think we should send them to the medical wing..");
+
+            }
+            else {
+                System.out.println("Dr. "+name+", the only two cooks here are Gummy or Lava!");
+                scan = new Scanner(System.in); //scanner reuse
+                System.out.println("Do you choose Gummy or Lava as today's cook?");
+            }
+            break;
+        }
+        scan.close(); //scanner closes 
     }
 }
--- Operator.java
+++ Operator.java
@@ -1,10 +1,38 @@
 public class Operator {
+    //instance variables
     private String name;
-    private int rarity;
+    private int level;
     private int potential;
     private int attack;
     private int defense;
     private double redeploymentTime;
 
+    Operator() {
+        level = 1;
+    }
 
+    //methods
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRedeploymentTime(double redeploymentTime){
+        this.redeploymentTime = redeploymentTime;
+    }
+
+    public double getRedeploymentTime(){
+        return redeploymentTime;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    
+    public void getAttack(){
+        System.out.println(name+" has "+attack+" attack.");
+    }
 }
