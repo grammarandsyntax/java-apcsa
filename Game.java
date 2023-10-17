f9cafdb2257e534a9430229462a5e7791744d518
grammarandsyntax <yevak2@nycstudents.net>
2023-10-17 09:51:50 -0400

    added escape sequences

--- Game.java
+++ Game.java
@@ -32,11 +32,11 @@ public class Game {
         System.out.println("Please input your name:");
         Scanner scan = new Scanner(System.in); //scanner opens
         String name=scan.nextLine();
-        System.out.println("Good morning Dr. "+name+". I hope you slept well, we've got a lot of work ahead of us!");
+        System.out.println("Good morning Dr. "+name+". I hope you slept well, we've got a lot of work ahead of us!\n");
         Wait(3000);
     }
     public void printCurrency(){
-        System.out.println("I'll start off with a recording of your current LMD and Orundum counts.");
+        System.out.println("I'll start off with a recording of your current LMD and Orundum counts.\n");
 
         Wait(2000);
 
@@ -50,19 +50,19 @@ public class Game {
 
         //ask to pull
         if (askHireOperator.equals("yes")){ //want to pull
-            System.out.println("Alright! That'll be 6000 orundum spent, which means... ");
+            System.out.println("\nAlright! That'll be 6000 orundum spent, which means... ");
             numOrundum = numOrundum-6000; //substract
 
             Wait(3000);
 
-            System.out.println("There's "+numOrundum+" orundum left.");
+            System.out.println("There's "+numOrundum+" orundum left.\n");
             Operator Texas = new Operator("Texas",219,18.0);
 
             System.out.println(Texas.getName()+" was hired! She would however like some compensation for her work and training. The total amount will be half our current LMD amount.");
 
             Wait(4500);
 
-            System.out.println("You accept the charges? Alright, I'll send the LMD to her.");
+            System.out.println("You accept the charges? Alright, I'll send the LMD to her.\n");
             numLmd/=2;
             numSixStarOperators++;
             System.out.println("There is now "+numLmd+" LMD in our holdings, and the current number of Top Operators is "+numSixStarOperators+".");
@@ -74,29 +74,26 @@ public class Game {
             String askInfo = scan.nextLine().toLowerCase();
 
             if (askInfo.equals("yes")){ //want infodump
-                System.out.println("After returning from her mission in Siracusa, it is "+isSixStar+" that she has gained more experience in battle. Her redeployment time has been reduced heavily to "+Texas.getRedeploymentTime()+" seconds. It increases by "+(Texas.getRedeploymentTime()/4)+" seconds on each new redeployment.");
+                System.out.println("\nAfter returning from her mission in Siracusa, it is "+isSixStar+" that she has gained more experience in battle. Her redeployment time has been reduced heavily to "+Texas.getRedeploymentTime()+" seconds. It increases by "+(Texas.getRedeploymentTime()/4)+" seconds on each new redeployment.");
                 Texas.getAttack();
 
                 Wait(8000);
             } else {
-                System.out.println("Ok.. but remember, it's important to learn about your operators to learn how to most effectively command them in battle!");
+                System.out.println("\nOk.. but remember, it's important to learn about your operators to learn how to most effectively command them in battle!");
                 Wait(1500);
                 System.out.println("Anyway, I'll go sort out Texas's files.");
             }
         }else{ //don't want to pull
-            System.out.println("That's alright, we have other matters to handle anyway!");
+            System.out.println("\nThat's alright, we have other matters to handle anyway!");
         }
     }
     public void waitLater(){
         Wait(3000);
-        System.out.println(" ");
-        System.out.println("A bit later...");
-        System.out.println(" ");
+        System.out.println("\nA bit later...\n");
         Wait(2000);
     }
     public void askAboutCook(){
-        System.out.println("Doctor, while I was working, I overheard some operators in the cafeteria arguing... could you please help settle it? I think it's over who has to cook..?");
-        System.out.println(" ");
+        System.out.println("Doctor, while I was working, I overheard some operators in the cafeteria arguing... could you please help settle it? I think it's over who has to cook..?\n");
 
         while(true){ //choose bwteen gum and lava
             System.out.println("Do you choose Gummy or Lava as today's cook?");
@@ -107,7 +104,7 @@ public class Game {
             if (chooseCook.equals("gummy")){ //YES GOOD
 
                 Wait(3000);
-                System.out.println("It seems the operators like her cooking! How will you divide the food among them though..?");
+                System.out.println("\nIt seems the operators like her cooking! How will you divide the food among them though..?");
                 sc = new Scanner(System.in); //scanner reuse
 
                 System.out.println("(How many whole pounds of food will you allocate?)");
@@ -131,7 +128,7 @@ public class Game {
             else if (chooseCook.equals("lava")){ //booo
 
                 Wait(3000);
-                System.out.println("It seems the operators don't like her cooking... I think we should send them to the medical wing..");
+                System.out.println("\nIt seems the operators don't like her cooking... I think we should send them to the medical wing..");
 
                 break;
             }
