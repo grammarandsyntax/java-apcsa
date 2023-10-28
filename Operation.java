562aff9ef8731d524305b62c30258cbba41d4b50
grammarandsyntax <144465898+grammarandsyntax@users.noreply.github.com>
2023-10-28 15:00:14 -0400

    Added Math class

--- Operation.java
+++ Operation.java
@@ -26,15 +26,18 @@ public class Operation {
         System.out.println("");
         Wait(3000);
     }
+    public int randomNummy(int min, int max){
+        return (int)(Math.random()*(max-min+1))+min;
+    }
 
     public void startOperation(int l){
-        Random rand = new Random();
-        int chanceWon = rand.nextInt(100);
+        int chanceWon = randomNummy(1,100);
+
         Wait(3000);
         if (chanceWon > 15){ //operation won
             System.out.println("Doctor, we won! I know we can handle anything with you around! Now let's see what we've obtained!\n");
             Wait(2000);
-            int obtainedLmd = rand.nextInt(1,450);
+            int obtainedLmd = randomNummy(1,450);
             if (obtainedLmd >= 200){
                 System.out.println("Nice, we picked up "+obtainedLmd+" Lmd!");
             } else {
@@ -42,7 +45,7 @@ public class Operation {
             }
             l+= obtainedLmd;
             System.out.println("\nDid we get any extra materials?\n");
-                int obtainedMaterials = rand.nextInt(100);
+                int obtainedMaterials = randomNummy(1,100);
                 if (obtainedMaterials>65){
                     System.out.println("Closure will find this oriron particularly useful!");
                 } else {
